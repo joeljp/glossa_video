d376944d622bbec76b813ad7130d942936b78ccd
joeljp <joeljp@gmail.com>
2014-02-07 12:07:31 +0100

    initial

---
+++ data4.js
@@ -0,0 +1,173 @@
+var mediaObj = {
+	"title":"\u212bseral - 3 & 4",
+	"last_line":"6",
+	"start_at":"3",
+	"end_at":"5",
+	"display_attribute":"ort",
+	"mov":{
+		"supplied":"m4v",
+		"path":"http://tekstlab.uio.no/jplayer/",
+//		"path":"rtmp://stream-prod01.uio.no/vod/mp4:uio/hf/ilf/scandiasyn/",
+		"movie_loc":"aaseral_03gm-04gk_800.mp4", //"drevsjoe_01um-02uk_800.mp4"
+		"start":"386.56",
+		"stop":"410.2"
+	},
+	"divs":{
+		"annotation":{
+			"0":{
+				"speaker":"aaseral_03gm",
+				"line":{
+					"0":{"ort":"jeg","lemma":"jeg","phon":"eg"},
+					"1":{"ort":"var"},
+					"2":{"ort":"inn"},
+					"3":{"ort":"p\u00e5"},
+					"4":{"ort":"posthuset"},
+					"5":{"ort":"og"},
+					"6":{"ort":"s\u00e5"},
+					"7":{"ort":"sto"},
+					"8":{"ort":"de"},
+					"9":{"ort":"innforbi"},
+					"10":{"ort":"og"},
+					"11":{"ort":"leste"},
+					"12":{"ort":"opp"},
+					"13":{"ort":"#"},
+					"14":{"ort":"alle"},
+					"15":{"ort":"brevene"},
+					"16":{"ort":"#"},
+					"17":{"ort":"og"},
+					"18":{"ort":"s\u00e5"},
+					"19":{"ort":"svarer"},
+					"20":{"ort":"du"},
+					"21":{"ort":"anf"},
+					"22":{"ort":"ja"},
+					"23":{"ort":"anf"},
+					"24":{"ort":"n\u00e5r"},
+					"25":{"ort":"de"},
+					"26":{"ort":"n\u00e5dde"},
+					"27":{"ort":"ut"},
+					"28":{"ort":"(uninterpretable)"}
+				},
+				"from":"386.56",
+				"to":"394.692"
+			},
+			"1":{
+				"speaker":"aaseral_04gk",
+				"line":{
+					"0":{"ort":"*"},
+					"1":{"ort":"\u00e5"},
+					"2":{"ort":"*"},
+					"3":{"ort":"kom"},
+					"4":{"ort":"til"},
+					"5":{"ort":"deg"}
+				},
+				"from":"386.56",
+				"to":"394.692"
+			},
+			"2":{
+				"speaker":"aaseral_03gm",
+				"line":{
+					"0":{"ort":"ja", "lemma":"ja","phon":"ja","pos":"interj","start":"start"},
+					"1":{"ort":"for"},
+					"2":{"ort":"de"},
+					"3":{"ort":"visste"},
+					"4":{"ort":"ikke"},
+					"5":{"ort":"hvem"},
+					"6":{"ort":"var"},
+					"7":{"ort":"der"}
+				},
+				"from":"394.692",
+				"to":"396.027"
+			},
+			"3":{
+				"speaker":"aaseral_04gk",
+				"line":{
+					"0":{"ort":"nei", "phon":"n\u00e6i", "lemma":"nei", "pos":"interj","start":"1"},
+					"1":{"ort":"det", "phon":"de", "lemma":"det", "pos":"pron:det"},
+					"2":{"ort":"veit", "phon":"v\u00e6it", "lemma":"vite","pos":"verb","temp":"pres"},
+					"3":{"ort":"du", "obs":"missing in transcrition"},
+					"4":{"ort":"det", "phon":"de", "lemma":"det", "pos":"pron:det"},
+					"5":{"ort":"er", "lemma":"v\u00e6re", "phon":"e", "pos":"verb","temp":"pres"},
+					"6":{"ort":"klart", "lemma":"klar", "phon": "klart", "pos":"adj", "gender":"neut", "num":"sg", "defn":"indef", "degr":"pos"}
+				},
+				"from":"396.027",
+				"to":"397.53"
+			},
+			"4":{
+				"speaker":"aaseral_03gm",
+				"line":{
+					"0":{"ort":"og","lemma":"og","phon":"\u00e5","pos":"conj","start":"1"},
+					"1":{"ort":"s\u00e5","lemma":"s\u00e5","phon":"s\u00e5","pos":"conj:subjunc:adv"},
+					"2":{"ort":"var","lemma":"v\u00e6re","phon":"va","pos":"verb","temp":"pret"},
+					"3":{"ort":"det","lemma":"det","phon":"de","pos":"pron","gender":"neut","num":"sg","type":"pers","pers":"3."},
+					"4":{"ort":"da","lemma":"da","phon":"d\u00e5","pos":"adv"},
+					"5":{"ort":"\u00e5","lemma":"\u00e5","phon":"\u00e5","pos":"inf-marker"},
+					"6":{"ort":"f\u00e5","lemma":"f\u00e5","phon":"f\u00e5","pos":"verb","mood":"inf"},
+					"7":{"ort":"dette","lemma":"dette","phon":"dette","pos":"verb","mood":"inf", "match" : "1"},
+					"8":{"ort":"her","lemma":"her","phon":"her","pos":"prep"},
+					"9":{"ort":"f\u00e5","lemma":"f\u00e5","phon":"f\u00e5","pos":"verb","mood":"inf"},
+					"10":{"ort":"det","lemma":"det","phon":"de","pos":"pron","gender":"neut","num":"sg","type":"pers","pers":"3."},
+					"11":{"ort":"med","lemma":"med","phon":"me","pos":"prep"},
+					"12":{"ort":"seg","lemma":"seg","phon":"seg","pos":"pron","type":"refl"},
+					"13":{"ort":"heim","lemma":"heim","phon":"h\u00e6im","pos":"noun","gender":"masc:fem","num":"sg","type":"cm-noun","defn":"indef"},
+					"14":{"ort":"att","lemma":"att","phon":"atte","pos":"noun","gender":"neut","num":"sg","type":"cm-noun|def"}
+				},
+				"from":"397.53",
+				"to":"400.32"
+			},
+			"5":{
+				"speaker":"aaseral_04gk",
+				"line":{
+					"0":{"ort":"\u00e5", "phon":"n\u00e6i", "lemma":"nei", "pos":"interj","start":"1"},
+					"1":{"ort":"ja", "phon":"de", "lemma":"det", "pos":"pron:det"},
+					"2":{"ort":"nei", "phon":"v\u00e6it", "lemma":"vite","pos":"verb","temp":"pres"},
+					"3":{"ort":"der", "phon":"de", "lemma":"det", "pos":"pron:det"},
+					"4":{"ort":"var", "lemma":"v\u00e6re", "phon":"e", "pos":"verb","temp":"pres"},
+					"5":{"ort":"oink", "lemma":"klar", "phon": "klart", "pos":"adj", "gender":"neut", "num":"sg", "defn":"indef", "degr":"pos"},
+					"6":{"ort":"delte"},
+					"7":{"ort":"alltid"},
+					"8":{"ort":"med"},
+					"9":{"ort":"postkassa"},
+					"10":{"ort":"og"},
+					"11":{"ort":"delte"},
+					"12":{"ort":"ut"},
+					"13":{"ort":"posten"},
+					"14":{"ort":"til"},
+					"15":{"ort":"heile"},
+					"16":{"ort":"garden"},
+					"17":{"ort":"#"},
+					"18":{"ort":"og"},
+					"19":{"ort":"s\u00e5"},
+					"20":{"ort":"var"},
+					"21":{"ort":"det"},
+					"22":{"ort":"opp"},
+					"23":{"ort":"til"},
+					"24":{"ort":"oink"},
+					"25":{"ort":"og"},
+					"26":{"ort":"Rei\u00e5sdalen"},
+				},
+				"from":"400.32",
+				"to":"407.617"
+			},
+			"6":{
+				"speaker":"aaseral_03gm",
+				"line":{
+					"0":{"ort":"oi","lemma":"oi","phon":"\u00e5","pos":"tull","start":"1"},
+					"1":{"ort":"grynt","lemma":"s\u00e5","phon":"s\u00e5","pos":"conj:subjunc:adv"},
+					"2":{"ort":"hrmph","lemma":"v\u00e6re","phon":"va","pos":"verb","temp":"pret"},
+					"3":{"ort":"Zzzz","lemma":"det","phon":"de","pos":"pron","gender":"neut","num":"sg","type":"pers","pers":"3."},
+					"4":{"ort":"snufs","lemma":"da","phon":"d\u00e5","pos":"adv"},
+					"5":{"ort":"faen","lemma":"\u00e5","phon":"\u00e5","pos":"inf-marker"},
+					"6":{"ort":"ja","lemma":"f\u00e5","phon":"f\u00e5","pos":"verb","mood":"inf"},
+					"7":{"ort":"ja","lemma":"ja","phon":"ia"},
+					"8":{"ort":"ja","lemma":"ja","phon":"ia"},
+					"9":{"ort":"ja","lemma":"ja","phon":"ia"},
+					"10":{"ort":"ja","lemma":"ja","phon":"ia"},
+					"11":{"ort":"ja","lemma":"ja","phon":"ia"},
+					"12":{"ort":"ja","lemma":"ja","phon":"ia"},
+				},
+				"from":"407.617",
+				"to":"410.2"
+			}
+		}
+	}
+};
